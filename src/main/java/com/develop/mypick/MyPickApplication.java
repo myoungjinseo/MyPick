38b76879548ba9adece653b47d6fc07af3af6896
MyoungJinSeo <sih5673@gmail.com>
2024-04-28 21:19:38 +0900

    Start Setting

---
+++ src/main/java/com/develop/mypick/MyPickApplication.java
@@ -0,0 +1,13 @@
+package com.develop.mypick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPickApplication.class, args);
+    }
+
+}
