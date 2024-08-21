47084e73996ad9df45b6944bfe9d50f770d0932e
MyoungJinSeo <sih5673@gmail.com>
2024-08-21 23:16:38 +0900

    ref : 의존성 변경

--- src/main/java/com/develop/mypick/api/Recommended/controller/RecommendedController.java
+++ src/main/java/com/develop/mypick/api/Recommended/controller/RecommendedController.java
@@ -1,8 +1,9 @@
 package com.develop.mypick.api.Recommended.controller;
 
-import com.develop.mypick.api.Recommended.dto.response.RecommendedResponse;
-import com.develop.mypick.api.Recommended.service.RecommendedService;
+import com.develop.mypick.api.Recommended.dto.response.ChatGptResponse;
+import com.develop.mypick.api.Recommended.service.ChatGptService;
 import com.develop.mypick.domain.user.entity.AuthUser;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -12,11 +13,11 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class RecommendedController {
 
-    private final RecommendedService recommendedService;
+    private final ChatGptService chatGptService;
 
     @GetMapping("/")
-    public RecommendedResponse getRecommend(@AuthenticationPrincipal AuthUser user) {
-        RecommendedResponse response = recommendedService.createRecommend(user);
+    public ChatGptResponse getRecommend(@AuthenticationPrincipal AuthUser user) throws JsonProcessingException {
+        ChatGptResponse response = chatGptService.sendChatGpt(user);
         return response;
     }
 }
