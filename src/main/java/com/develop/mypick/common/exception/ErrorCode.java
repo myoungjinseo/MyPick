6c4dcedc9632f8bb22f1a059c291c4c60642bb04
MyoungJinSeo <sih5673@gmail.com>
2024-08-23 15:59:10 +0900

    feat : json 에러 코드 추가

--- src/main/java/com/develop/mypick/common/exception/ErrorCode.java
+++ src/main/java/com/develop/mypick/common/exception/ErrorCode.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+    //404 에러가 아닌 400 에러 USER 정보가 없다는 정보를 주지 않기위해
+    NOT_FOUND_USER(HttpStatus.BAD_REQUEST,"존재하지 않는 유저 정보입니다"),
+    CHATGPT_ERROR(HttpStatus.BAD_REQUEST,"ChatGPT 자체 오류입니다"),
     EXITS_EMAIL(HttpStatus.BAD_REQUEST,"이미 존재하는 이메일입니다"),
     EXITS_USER_PHYSICAL(HttpStatus.BAD_REQUEST,"유저 피지컬이 이미 존재합니다"),
     LOGIN_FAIL(HttpStatus.BAD_REQUEST,"아이디 혹은 비밀번호를 확인하세요.");
