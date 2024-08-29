5d351a9c196b968d633b5d3ffbd68ea404b0b9c9
MyoungJinSeo <sih5673@gmail.com>
2024-08-29 14:55:55 +0900

    ref : 유저 이름 중복 가능

--- src/main/java/com/develop/mypick/domain/user/entity/AuthUser.java
+++ src/main/java/com/develop/mypick/domain/user/entity/AuthUser.java
@@ -21,7 +21,7 @@ public class AuthUser extends BaseTimeEntity implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String username;
 
     @Column(nullable = false, unique = true)
