a8652dace403d6cb8f614b5519d5e53f4d129aeb
MyoungJinSeo <sih5673@gmail.com>
2024-08-30 02:26:52 +0900

    feat : 예외처리 추가

--- src/main/java/com/develop/mypick/common/exception/ErrorCode.java
+++ src/main/java/com/develop/mypick/common/exception/ErrorCode.java
@@ -7,11 +7,24 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+    //Auth
+    AUTH_NOT_FOUND(HttpStatus.UNAUTHORIZED, "시큐리티 인증 정보를 찾을 수 없습니다."),
+    UNKNOWN_ERROR(HttpStatus.UNAUTHORIZED, "알 수 없는 에러"),
+    EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 access Token입니다"),
+    EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 refresh Token입니다. 재로그인하세요"),
+    UNSUPPORTED_TOKEN(HttpStatus.UNAUTHORIZED, "토큰 길이 및 형식이 다른 Token입니다"),
+    WRONG_TYPE_TOKEN(HttpStatus.UNAUTHORIZED, "서명이 잘못된 토큰입니다."),
+    ACCESS_DENIED(HttpStatus.UNAUTHORIZED, "토큰이 없습니다"),
+    TOKEN_SUBJECT_FORMAT_ERROR(HttpStatus.UNAUTHORIZED, "Subject 값에 Long 타입이 아닌 다른 타입이 들어있습니다."),
+    AT_EXPIRED_AND_RT_NOT_FOUND(HttpStatus.UNAUTHORIZED, "AT는 만료되었고 RT는 비어있습니다."),
+    RT_NOT_FOUND(HttpStatus.UNAUTHORIZED, "RT가 비어있습니다"),
+
+
     NOT_FOUND_RECOMMENDED(HttpStatus.NOT_FOUND,"찾을 수 없는 추천 정보입니다."),
     //404 에러가 아닌 400 에러 USER 정보가 없다는 정보를 주지 않기위해
     NOT_FOUND_USER(HttpStatus.BAD_REQUEST,"존재하지 않는 유저 정보입니다"),
     CHATGPT_ERROR(HttpStatus.BAD_REQUEST,"ChatGPT 자체 오류입니다"),
-    EXITS_EMAIL(HttpStatus.BAD_REQUEST,"이미 존재하는 이메일입니다"),
+    EXITS_EMAIL(HttpStatus.BAD_REQUEST,"이미 존재하는 이메일입니다."),
     EXITS_USER_PHYSICAL(HttpStatus.BAD_REQUEST,"유저 피지컬이 이미 존재합니다"),
     LOGIN_FAIL(HttpStatus.BAD_REQUEST,"아이디 혹은 비밀번호를 확인하세요.");
 
---
+++ src/main/java/com/develop/mypick/common/jwt/JwtExceptionFilter.java
@@ -0,0 +1,52 @@
+package com.develop.mypick.common.jwt;
+
+import com.develop.mypick.common.exception.ErrorCode;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class JwtExceptionFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        response.setCharacterEncoding("utf-8");
+        try {
+            filterChain.doFilter(request, response);
+        } catch (JwtException ex) {
+            setResponse(response, getJwtExceptionErrorCode(ex));
+        } catch (NumberFormatException ex) {
+            setResponse(response, ErrorCode.TOKEN_SUBJECT_FORMAT_ERROR);
+        }
+    }
+    private ErrorCode getJwtExceptionErrorCode(JwtException ex) {
+        ErrorCode[] errorCodes = {
+                ErrorCode.UNKNOWN_ERROR,
+                ErrorCode.WRONG_TYPE_TOKEN,
+                ErrorCode.EXPIRED_ACCESS_TOKEN,
+                ErrorCode.EXPIRED_REFRESH_TOKEN,
+                ErrorCode.UNSUPPORTED_TOKEN
+        };
+
+        return Arrays.stream(errorCodes)
+                .filter(errorCode -> errorCode.getMessage().equals(ex.getMessage()))
+                .findFirst()
+                .orElse(ErrorCode.ACCESS_DENIED);
+    }
+    private void setResponse(HttpServletResponse response, ErrorCode errorCode) {
+        try {
+            logger.error(errorCode.getMessage());
+            response.setContentType("application/json;charset=UTF-8");
+            response.setStatus(errorCode.getHttpStatus().value());
+            response.getWriter().print(errorCode.getMessage());
+        } catch (IOException e) {
+            logger.error("Error setting response : ", e);
+        }
+    }
+}
